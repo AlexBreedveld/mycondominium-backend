3bef807b4bed9c86f7963a70327404f2f2bf05f9
joaorajao <joaorajao5@gmail.com>
2025-04-26 23:31:58 -0300

    fix erro support

--- src/middleware/cors.rs
+++ src/middleware/cors.rs
@@ -4,8 +4,10 @@ use actix_web::http::header;
 
 pub fn cors() -> Cors {
     Cors::default()
-        .allow_any_origin()
-        .allow_credentials(true)
+        
+        .allowed_origin("http://localhost:3000")
+        
+        .supports_credentials()
         .allowed_methods(vec!["GET", "POST", "PUT", "DELETE", "OPTIONS"])
         .allowed_headers(vec![
             header::AUTHORIZATION,
